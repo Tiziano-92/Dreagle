f94b6daf6d55c2e649848b0f1300c91cdd3fb31f
ferraricharles <charles.cjf@gmail.com>
2016-05-20 00:34:19 +0200

    starting implementing config controler

---
+++ configuration.js
@@ -0,0 +1,37 @@
+var config = require('./config/config.json');
+
+exports.localReg = function() {
+	
+	var system_status = config.status;
+	var whitelist = config.whitelist;
+	var push_it_away = config.actions.pia;
+	var custom_landing = config.actions.cls;
+	var logs_link = config.firmware.logs;
+	return config
+}
+
+exports.get_system_status = function(){
+	return config.status;
+}
+
+exports.get_whitelist = function(){
+	return config.whitelist;
+}
+
+exports.get_push_it_away = function(){
+	return config.actions.pia;
+}
+
+exports.get_custom_landing = function(){
+	return config.actions.cls;
+}
+
+exports.get_logs_link = function(){
+	return config.firmware.logs;
+}
+
+var user = {
+    "username": " aurhauhra",
+    "password": "hehehehe",
+    "avatar": "http://placepuppy.it/images/homepage/Beagle_puppy_6_weeks.JPG"
+  }
